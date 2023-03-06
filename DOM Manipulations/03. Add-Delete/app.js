3a226270e3dd7620759fad06341b7241b6505c22
cheren211 <p.bachiyskii@gmail.com>
2023-03-06 23:58:15 +0200

    Update app.js

--- DOM Manipulations/03. Add-Delete/app.js
+++ DOM Manipulations/03. Add-Delete/app.js
@@ -1,3 +1,16 @@
 function addItem() {
-    //TODO...
+  const newItems = document.getElementById("newItemText");
+  const items = document.getElementById("items");
+  const li = document.createElement("li");
+  li.textContent = newItems.value;
+  items.appendChild(li);
+  newItems.value = "";
+  const deleteButton = document.createElement("a");
+  deleteButton.textContent = "[Delete]";
+  deleteButton.href = "#";
+  li.appendChild(deleteButton);
+  deleteButton.addEventListener("click", delItem);
+  function delItem(event) {
+    event.target.parentNode.remove();
+  }
 }
