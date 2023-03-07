6759cbc8b35e725caa51cd8042f68f34e5f75432
cheren211 <p.bachiyskii@gmail.com>
2023-03-07 21:41:37 +0200

    Update app.js

--- DOM Manipulations/04. Mouse-Gradient/app.js
+++ DOM Manipulations/04. Mouse-Gradient/app.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  const gradient = document.getElementById("gradient");
+  gradient.addEventListener("mousemove", move);
+  const result = document.getElementById("result");
+  function move(ev) {
+    let sum = Math.floor((ev.offsetX / gradient.clientWidth) * 100);
+    result.textContent = `${sum}%`;
+  }
 }
