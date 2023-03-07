9688ca447bbfccce67c146f160afa231eea585a4
cheren211 <p.bachiyskii@gmail.com>
2023-03-07 22:15:08 +0200

    Update app.js

--- DOM Manipulations/05. Highlight-Active/app.js
+++ DOM Manipulations/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+  const textFields = Array.from(document.querySelectorAll("input"));
+  for (const text of textFields) {
+    text.addEventListener("focus", onClick);
+    text.addEventListener("blur", onBlur);
+  }
+
+  function onClick(ev) {
+    ev.target.parentElement.classList = "focused";
+  }
+  function onBlur(ev) {
+    ev.target.parentElement.classList = "";
+  }
 }
